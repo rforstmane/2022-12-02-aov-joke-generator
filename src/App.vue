9c09713f9bc641d644b21480cbc55b77900172ad
Renda Forstmane <rf@longenesis.com>
2022-12-04 14:36:06 +0200

    Solution: day2

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,40 @@
 <template>
-  <div class="w-full h-full flex justify-center items-center">
-    <span class="text-3xl">Good luck!</span>
+  <div class='w-full h-full flex justify-center items-center'>
+    <div class='max-w-xs w-full flex flex-col'>
+      <template v-if='!isLoading'>
+        <div class='w-3/4 p-4 rounded-2xl bg-teal-800 text-white self-start mb-4'>{{ joke }}</div>
+        <div class='w-3/4 p-4 rounded-2xl bg-red-800 text-white self-end' v-if='showAnswer'>{{ jokeAnswer }}</div>
+        <button v-if='!showAnswer' @click='showAnswer = true'
+                class='bg-green col-span-1 rounded-lg py-2 hover:opacity-90 w-full mx-auto mt-4'>
+          Tell Me!
+        </button>
+        <button v-if='showAnswer' @click='getJoke'
+                class='bg-green col-span-1 rounded-lg py-2 hover:opacity-90 w-full mx-auto mt-4'>
+          Another
+        </button>
+      </template>
+    </div>
   </div>
 </template>
 
 <script setup>
-// fetch('https://v2.jokeapi.dev/joke/christmas') ...
+import { ref } from 'vue'
+
+const joke = ref(null)
+const jokeAnswer = ref(null)
+const showAnswer = ref(false)
+const isLoading = ref(true)
+
+const getJoke = () => {
+  showAnswer.value = false
+  joke.value = null
+  jokeAnswer.value = null
+  fetch('https://v2.jokeapi.dev/joke/christmas').then(res => res.json()).then(data => {
+    joke.value = data.setup
+    jokeAnswer.value = data.delivery
+  }).finally(() => {
+    isLoading.value = false
+  })
+}
+getJoke()
 </script>
